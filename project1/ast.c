8ef8d8845cd2039da0cbc9b5616fb29525e07e09
Eveneko <huyubin0203@gmail.com>
2020-10-10 20:12:06 +0000

    basic version, do not fix any bugs

--- project1/ast.c
+++ project1/ast.c
@@ -4,25 +4,54 @@
 #include <stdarg.h>
 #include "ast.h"
 
-typedef enum tokenType
-{
-    NONTERMINAL,
-    TOKENWITHVALUE,
-    TOKENWITHOUTVALUE
-}tokenType;
+astNode *newAst(char *astNodeName, tokenType astNodeType, char *astNodeValue, int lineno, int childNum, ...){
+    astNode *_astNode = malloc(sizeof(astNode));
 
-typedef struct astNode
-{
-    char* astNodeName;
-    tokenType astNodeType;
-    char* astNodeValue;
-    int lineno;
-    int childNum; 
-    struct astNode** childNodes;
-}astNode;
+    _astNode->astNodeName = astNodeName;
+    _astNode->astNodeType = astNodeType;
+    _astNode->astNodeValue = astNodeValue;
+    _astNode->lineno = lineno;
+    _astNode->childNum = childNum;
+    _astNode->childNodes = NULL;
 
-astNode* newAst(char* astNodeName, tokenType astNodeType, char* astNodeValue, int lineno, int childNum, ...);
-void printAst(astNode* node, int indentNum);
-void freeAst(astNode* node);
+    va_list args;
+    va_start(args, childNum);
 
-void indent(int n);
+    for(astNode **ptr = _astNode->childNodes; childNum--; ++ptr){
+        *ptr = va_arg(args, astNode*);
+    }
+
+    return _astNode;
+}
+
+void printAst(astNode *_astNode, int indentNum){
+    if(_astNode->astNodeType == NON_TERMINAL && _astNode->childNodes != NULL){
+        indent(indentNum);
+        fprintf(fout, "%s (%d)\n", _astNode->astNodeName, _astNode->lineno);
+    }
+    else if(_astNode->astNodeType == TOKEN_WITH_VALUE){
+        indent(indentNum);
+        fprintf(fout, "%s %s\n", _astNode->astNodeName, _astNode->astNodeValue);
+    }
+    else if(_astNode->astNodeType == TOKEN_WITHOUT_VALUE){
+        indent(indentNum);
+        fprintf(fout, "%s\n", _astNode->astNodeName);
+    }
+    for(int i = 0; i < _astNode->childNum; ++i){
+        printAst(_astNode->childNodes[i], indentNum + INDENT_NUM);
+    }
+}
+
+void freeAst(astNode *_astNode){
+    for(int i = 0; i < _astNode->childNum; ++i){
+        freeAst(_astNode->childNodes[i]);
+    }
+    free(_astNode);
+}
+
+void indent(int n){
+    while(n-- > 0){
+        fprintf(fout, " ");
+    }
+    return;
+}
--- project1/ast.h
+++ project1/ast.h
@@ -1,6 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <stdarg.h>
+#define INDENT_NUM 2
 
 FILE *fin, *fout;
+
+typedef enum tokenType{
+    NON_TERMINAL,
+    TOKEN_WITH_VALUE,
+    TOKEN_WITHOUT_VALUE
+}tokenType;
+
+typedef struct astNode{
+    char *astNodeName;
+    tokenType astNodeType;
+    char *astNodeValue;
+    int lineno;
+    int childNum; 
+    struct astNode **childNodes;
+}astNode;
+
+astNode *newAst(char *astNodeName, tokenType astNodeType, char *astNodeValue, int lineno, int childNum, ...);
+void printAst(astNode *_astNode, int indentNum);
+void freeAst(astNode *_astNode);
+
+void indent(int n);
